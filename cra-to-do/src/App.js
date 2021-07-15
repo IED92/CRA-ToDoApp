c349f06306091e19645343ee146d43b61663ec43
Ian <55965026+IED92@users.noreply.github.com>
2021-07-15 08:56:59 -0700

    Added create todo item functionality
    Added the ability to create new todo item.

--- cra-to-do/src/App.js
+++ cra-to-do/src/App.js
@@ -3,11 +3,33 @@ import './App.css';
 
 function Todo({ todo }) {
   return (
-    <div className="todo-item">
+    <div className = "todo-item">
       {todo.text}
     </div>
   );
 };
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = React.useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  }
+
+  return (
+    <form onSubmit = {handleSubmit}>
+      <input 
+        type = "text"
+        className = "input"
+        value = {value}
+        onChange = {e  => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
 
 function App() {
   const [todos, setTodos] = React.useState([
@@ -17,17 +39,22 @@ function App() {
     { text: "Profit"},
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
 
 return (
-  <div className="todo-app">
-    <div className="todo-list">
+  <div className = "todo-app">
+    <div className = "todo-list">
       {todos.map((todo, index) => (
         <Todo
-        key={index}
-        index={index}
-        todo={todo}
+        key = {index}
+        index = {index}
+        todo = {todo}
         /> 
       ))}
+      <TodoForm addTodo = {addTodo} />
     </div>
   </div> 
  );
